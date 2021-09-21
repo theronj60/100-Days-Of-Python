dd4d8d799ba152a4b1c29053e950faa129f7cd58
Theron Joe <theronj@joetwebdev.io>
2021-09-20 23:42:57 -0600

    started calc project.

--- day-10/calc.py
+++ day-10/calc.py
@@ -13,5 +13,18 @@ def divide(n1, n2):
 operations = {
     "+": add,
     "-": subtract,
-    "multiply": divide,
+    "*": multiply,
+    "/": divide,
 }
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operations from the line above: ") 
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
