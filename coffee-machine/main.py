ff3c84d3304c6274b0656332d57607a93f4670ab
Theron Joe <theronj@joetwebdev.io>
2022-07-14 00:38:09 -0600

    added new functions for coffee machine, getting closer to finish

--- coffee-machine/main.py
+++ coffee-machine/main.py
@@ -32,25 +32,49 @@ resources = {
 
 stillOrdering = True
 
+def check_resources():
+    return resources
+
+def get_recipe(drink): # pass in drinkOrder
+    recipe = MENU[drink]["ingredients"]
+    return recipe
+
+def make_drink(order): # pass in ingredients needed
+    resources["water"] - MENU[order]["ingredients"]["water"]
+    resources["milk"] - MENU[order]["ingredients"]["milk"]
+    resources["coffee"] - MENU[order]["ingredients"]["milk"]
+    return "Heres your "
+
+# type == water, milk or coffee
+# value == amount to add to type
+def refill_resource(type, value):
+    resources[type] = resources[type] + value
+    return resources
+
 while stillOrdering: 
     drinkOrder = input("What would you like? (espresso/latte/cappuccino): ")
     drinkOrder = drinkOrder.lower()
 
-    def drink(drink):
-        if (drink == 'espresso'):
-            return MENU['espresso']
-        elif (drink == 'latte'):
-            return MENU['latte']
-        elif (drink == 'cappuccino'):
-            return MENU['cappuccino']
-        else:
-            return "coffee will do"    
-
-    print(drink(drinkOrder))
-    still = input("would you like to order another? y/n: " )
-    if (still == 'n'):
-        stillOrdering = False
-        exit()
-    else:
+    if drinkOrder not in MENU:
+        print("Please select a listed drink.")
         continue
+    else:
+        def drink(drink):
+            if (drink == "espresso"):
+                return MENU["espresso"]["ingredients"] # test listing ingredient items
+            elif (drink == "latte"):
+                return MENU["latte"]
+            elif (drink == "cappuccino"):
+                return MENU["cappuccino"]
+            else:
+                return "coffee will do"    
+
+        print(drink(drinkOrder))
+
+        still = input("would you like to order another? y/n: " )
+        if (still == "n"):
+            stillOrdering = False
+            exit()
+        else:
+            continue
 
