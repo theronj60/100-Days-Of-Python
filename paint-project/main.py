6932abe13c31f2b0a715e6a6c40954eaaaea1a7d
Theron Joe <theronj@joetwebdev.io>
2022-08-23 22:54:34 -0600

    finished paint project, uses turtle library to recreate a 'hurst' painting

--- paint-project/main.py
+++ paint-project/main.py
@@ -1,6 +1,16 @@
 import colorgram
+import random
+import turtle as turtle_module
 
-hurst_colors = colorgram.extract('image.jpg', 12)
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+screen = turtle_module.Screen()
+screen.setworldcoordinates(-50,-50,500,500)
+tim.up()
+tim.speed('fastest')
+tim.hideturtle()
+
+hurst_colors = colorgram.extract('image.jpg', 30)
 
 def get_rgb(cg_colors):
     colors = []
@@ -9,9 +19,27 @@ def get_rgb(cg_colors):
         color_tuple = (color.rgb.r, color.rgb.g, color.rgb.b)
         colors.append(color_tuple)
 
-    # need to check tuples within list
-    # check for white colors within
-    # remove white values
     return colors
-    
-print(get_rgb(hurst_colors))
+ 
+def draw_row(row_len):
+    for i in range(row_len):
+        tim.dot(20, random.choice(get_rgb(hurst_colors)))
+        if i < row_len - 1: 
+            tim.forward(50)
+
+for c in range(10):
+    if c == 0:
+        draw_row(10)
+    else:
+        if tim.xcor() > 0:
+            tim.left(90)
+            tim.forward(50)
+            tim.left(90)    
+            draw_row(10)
+        else:
+            tim.right(90)
+            tim.forward(50)
+            tim.right(90)
+            draw_row(10)
+
+screen.exitonclick()
